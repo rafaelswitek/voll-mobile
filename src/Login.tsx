7b15e53d62e618b461fac2e23097b4e29511f431
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-04 23:56:02 -0300

    02

---
+++ src/Login.tsx
@@ -0,0 +1,61 @@
+import { Box, Button, FormControl, Image, Input, Link, Text, VStack } from 'native-base'
+import Logo from './assets/Logo.png'
+import { TouchableOpacity } from 'react-native';
+import { Titulo } from './componentes/Titulo';
+
+export default function Login() {
+  return (
+    <VStack flex={1} alignItems="center" p={5}>
+      <Image source={Logo} alt="Logo Voll" />
+      <Titulo>
+        Faça login em sua conta
+      </Titulo>
+      <Box>
+        <FormControl mt={3}>
+          <FormControl.Label>Email</FormControl.Label>
+          <Input
+            placeholder='Insira seu endereço de email'
+            size='lg'
+            w="100%"
+            borderRadius='lg'
+            bgColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+        <FormControl mt={3}>
+          <FormControl.Label>Senha</FormControl.Label>
+          <Input
+            placeholder='Insira sua senha'
+            size='lg'
+            w="100%"
+            borderRadius='lg'
+            bgColor='gray.100'
+            shadow={3}
+          />
+        </FormControl>
+      </Box>
+
+      <Button
+        w="100%"
+        bg="blue.800"
+        mt={10}
+        borderRadius="lg"
+      >
+        Entrar
+      </Button>
+
+      <Link href='https://www.alura.com.br' mt={2}>
+        Esqueceu sua senha?
+      </Link>
+
+      <Box w="100%" flexDirection="row" justifyContent="center" mt={8}>
+        <Text>Ainda não tem cadastro? </Text>
+        <TouchableOpacity>
+          <Text color="blue.500">
+            Faça seu cadastro!
+          </Text>
+        </TouchableOpacity>
+      </Box>
+    </VStack>
+  );
+}
---
+++ src/componentes/Titulo.tsx
@@ -0,0 +1,21 @@
+import { ReactNode } from "react";
+import { ITextProps, Text } from "native-base";
+
+interface TituloProps extends ITextProps {
+    children: ReactNode
+}
+
+export function Titulo({ children, ...rest }: TituloProps) {
+    return (
+        <Text
+            fontSize="2xl"
+            fontWeight="bold"
+            color="gray.500"
+            textAlign="center"
+            mt={5}
+            {...rest}
+        >
+            {children}
+        </Text>
+    )
+}
