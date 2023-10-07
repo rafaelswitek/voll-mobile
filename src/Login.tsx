c5fc5037b9455421e44fe6e6a19bca7a7595fa5b
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-06 22:58:37 -0300

    09

--- src/Login.tsx
+++ src/Login.tsx
@@ -17,7 +17,6 @@ export default function Login({ navigation }) {
   const toast = useToast()
 
   useEffect(() => {
-    AsyncStorage.removeItem('token')
     async function verificarLogin() {
       const token = await AsyncStorage.getItem('token')
       if (token) {
--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,24 +1,57 @@
+import { useState, useEffect } from 'react'
 import { Text, VStack, ScrollView, Avatar, Divider } from "native-base";
 
 import { Titulo } from "../componentes/Titulo";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { pegarDadosPaciente } from "../servicos/PacienteServico";
+import { Paciente } from '../interfaces/Paciente';
+import { Botao } from '../componentes/Botao';
 
 export default function Perfil({ navigation }) {
+  const [dadosPaciente, setDadosPaciente] = useState({} as Paciente)
+
+  useEffect(() => {
+    async function dadosPaciente() {
+      const pacienteId = await AsyncStorage.getItem('pacienteId')
+      if (!pacienteId) return null
+
+      const resultado = await pegarDadosPaciente(pacienteId)
+      if (resultado) {
+        setDadosPaciente(resultado)
+      }
+    }
+    dadosPaciente()
+  }, [])
+
+  function deslogar() {
+    AsyncStorage.removeItem('token')
+    AsyncStorage.removeItem('pacienteId')
+    navigation.replace('Login')
+  }
+
   return (
     <ScrollView flex={1}>
       <VStack alignItems='center' p={5}>
         <Titulo color='blue.500'>Meu Perfil</Titulo>
 
-        <Avatar size='xl' source={{ uri: 'https://avatars.githubusercontent.com/u/6961415?v=4' }} mt={5} />
+        <Avatar size='xl' source={{ uri: dadosPaciente.imagem }} mt={5} />
         <Titulo>Informações Pessoais</Titulo>
-        <Titulo fontSize='lg' mb={1}>Rafael Gonçalves</Titulo>
-        <Text>11/12/1995</Text>
-        <Text>Anápolis-GO</Text>
+        <Titulo fontSize='lg' mb={1}>{dadosPaciente?.nome}</Titulo>
+        <Text>{dadosPaciente?.email}</Text>
+        <Text>{dadosPaciente?.endereco?.estado}</Text>
 
         <Divider mt={5} />
 
-        <Titulo color='blue.500' mb={1}>Histórico Medico</Titulo>
-        <Text>Bronquite</Text>
-        <Text>Sinusite</Text>
+        <Titulo color='blue.500' mb={1}>Planos de saúde</Titulo>
+        {
+          dadosPaciente?.planosSaude?.map((plano, index) => (
+            <Text key={index}>{plano}</Text>
+          ))
+        }
+
+        <Botao onPress={deslogar}>
+          Deslogar
+        </Botao>
       </VStack>
 
     </ScrollView>
