375f389a6d4ad9b61ec0ad778d38929bc1a8b202
Rafael Gonçalves <rafaelswitek@gmail.com>
2023-10-06 22:07:49 -0300

    07

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,11 +1,56 @@
-import { Box, Image, Link, Text, VStack } from 'native-base'
+import { useState, useEffect } from 'react'
+import { Box, Image, Link, Text, VStack, useToast } from 'native-base'
 import Logo from './assets/Logo.png'
 import { TouchableOpacity } from 'react-native';
 import { Titulo } from './componentes/Titulo';
 import { EntradaTexto } from './componentes/EntradaTexto';
 import { Botao } from './componentes/Botao';
+import { fazerLogin } from './servicos/AutenticacaoServico';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import jwtDecode from 'jwt-decode';
+
 
 export default function Login({ navigation }) {
+  const [email, setEmail] = useState('andre@email.com')
+  const [senha, setSenha] = useState('1234')
+  const [carregando, setCarregando] = useState(true)
+  const toast = useToast()
+
+  useEffect(() => {
+    AsyncStorage.removeItem('token')
+    async function verificarLogin() {
+      const token = await AsyncStorage.getItem('token')
+      if (token) {
+        navigation.replace('Tabs')
+      }
+      setCarregando(false)
+    }
+    verificarLogin()
+  }, [])
+
+  async function login() {
+    const resultado = await fazerLogin(email, senha)
+    if (resultado) {
+      const { token } = resultado
+      AsyncStorage.setItem('token', token)
+
+      const tokenDecodificado = jwtDecode(token) as any
+      const pacienteId = tokenDecodificado.id
+      AsyncStorage.setItem('pacienteId', pacienteId)
+      navigation.replace('Tabs')
+    } else {
+      toast.show({
+        title: "Erro no login",
+        description: "O email ou senha não conferem",
+        backgroundColor: "red.500"
+      })
+    }
+  }
+
+  if (carregando) {
+    return null
+  }
+
   return (
     <VStack flex={1} alignItems="center" p={5}>
       <Image source={Logo} alt="Logo Voll" />
@@ -16,14 +61,19 @@ export default function Login({ navigation }) {
         <EntradaTexto
           label="Email"
           placeholder="Insira seu endereço de e-mail"
+          value={email}
+          onChangeText={setEmail}
         />
 
         <EntradaTexto
           label="Senha"
+          value={senha}
+          onChangeText={setSenha}
+          secureTextEntry
         />
       </Box>
 
-      <Botao onPress={() => navigation.navigate('Tabs')}>
+      <Botao onPress={() => login()}>
         Entrar
       </Botao>
 
--- src/componentes/EntradaTexto.tsx
+++ src/componentes/EntradaTexto.tsx
@@ -3,9 +3,12 @@ import { FormControl, ITextProps, Input } from "native-base";
 interface EntradaTextoProps extends ITextProps {
     label?: string
     placeholder?: string
+    secureTextEntry?: boolean;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
 
-export function EntradaTexto({ label, placeholder }: EntradaTextoProps) {
+export function EntradaTexto({ label, placeholder, secureTextEntry = false, value, onChangeText }: EntradaTextoProps) {
     return (
         <FormControl mt={3}>
             {label && <FormControl.Label>{label}</FormControl.Label>}
@@ -16,6 +19,9 @@ export function EntradaTexto({ label, placeholder }: EntradaTextoProps) {
                 borderRadius='lg'
                 bgColor='gray.100'
                 shadow={3}
+                value={value}
+                onChangeText={onChangeText}
+                secureTextEntry={secureTextEntry}
             />
         </FormControl>
     )
